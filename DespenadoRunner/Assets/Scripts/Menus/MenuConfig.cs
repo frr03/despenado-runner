f15a08fa8e5fdc8f7f9fc8fee837c07fcfd9fc82
frr03 <felipe.raposo@sga.pucminas.br>
2024-04-20 22:11:26 -0300

    Volume + UI

---
+++ DespenadoRunner/Assets/Scripts/Menus/MenuConfig.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MenuConfig : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("SavedVolume", 1.0f);
+        volumeSlider.value = savedVolume;
+        SetVolume(savedVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+
+        PlayerPrefs.SetFloat("SavedVolume", volume);
+        PlayerPrefs.Save();
+    }
+}
