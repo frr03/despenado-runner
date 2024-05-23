c4054114db957ab402616042e417e986b81f54ab
frr03 <felipe.raposo@sga.pucminas.br>
2024-05-23 01:45:03 -0300

    Otimização PlayerController + Egg

--- DespenadoRunner/Assets/Scripts/Player/PlayerController.cs
+++ DespenadoRunner/Assets/Scripts/Player/PlayerController.cs
@@ -13,22 +13,34 @@ public class PlayerController : MonoBehaviour
 
     // PowerUp Milho
     private bool speedBoost = false;
-    public float count_M;
-    public float milho;
+    private float count_M;
 
     // PowerUp Escudo
-    public float count_e;
-    public float res;
+    private float count_e;
+    private bool hasShield = false;
     public GameObject escudo;
 
     // Perder Velocidade ao tomar Dano
     private bool speedDown = false;
-    public float count_D;
-    public float getHit;
+    private float count_D;
+
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
 
     void Update()
     {
-        // Velocidades
+        UpdateMovementSpeed();
+        UpdateShield();
+        UpdateSpeedBoost();
+        UpdateSpeedDown();
+    }
+
+    private void UpdateMovementSpeed()
+    {
         if (speedBoost && !speedDown)
         {
             ml.VelocidadeBoost();
@@ -41,124 +53,129 @@ public class PlayerController : MonoBehaviour
         {
             ml.VelocidadeNormal();
         }
+    }
 
-        #region // ESCUDO
-        
-        if (res == 1)
+    private void UpdateShield()
+    {
+        if (hasShield)
         {
             escudo.SetActive(true);
+            count_e -= Time.deltaTime;
             if (count_e <= 0)
             {
-                FindObjectOfType<AudioManager>().Play("ShieldDestroy");
-                res = 0;
-                count_e = -1f;
+                audioManager.Play("ShieldDestroy");
+                hasShield = false;
+                escudo.SetActive(false);
             }
-            count_e -= Time.deltaTime;
         }
-        else if (res == 0)
-        {
-            escudo.SetActive(false);
-        }
+    }
 
-        #endregion
-
-        #region // MILHO
-        if (milho == 1)
+    private void UpdateSpeedBoost()
+    {
+        if (speedBoost)
         {
-            speedBoost = true;
             count_M -= Time.deltaTime;
             if (count_M <= 0)
             {
-                milho = 0;
                 speedBoost = false;
-                count_M = 0;
             }
         }
-        #endregion
+    }
 
-        #region // DIMINUIR VELOCIDADE AO TOMAR DANO
-        if (getHit == 1)
+    private void UpdateSpeedDown()
+    {
+        if (speedDown)
         {
-            speedDown = true;
             count_D -= Time.deltaTime;
             if (count_D <= 0)
             {
-                getHit = 0;
                 speedDown = false;
-                count_D = 0;
             }
         }
-        #endregion
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Milho"))
+        switch (other.tag)
         {
-            milho = 1;
-            count_M = 1.5f;
-            FindObjectOfType<AudioManager>().Play("Speed");
-            Destroy(other.gameObject);
+            case "Milho":
+                ActivateSpeedBoost();
+                Destroy(other.gameObject);
+                break;
+            case "Egg":
+                ActivateShield();
+                Destroy(other.gameObject);
+                break;
+            case "Pontuador":
+                Pontos.QtdPts += 2;
+                Destroy(other.gameObject);
+                break;
+            case "Limite":
+                HandleVictory();
+                break;
+            case "Item":
+                HandleItemPickup();
+                Destroy(other.gameObject);
+                break;
+            case "Obstaculo":
+                HandleObstacleCollision();
+                Destroy(other.gameObject);
+                break;
+            case "InstaKill":
+                _hc.Instakill();
+                break;
         }
-        
-        if (other.CompareTag("Egg"))
+    }
+
+    private void ActivateSpeedBoost()
+    {
+        speedBoost = true;
+        count_M = 1.5f;
+        audioManager.Play("Speed");
+    }
+
+    private void ActivateShield()
+    {
+        hasShield = true;
+        count_e = 5f;
+    }
+
+    private void HandleVictory()
+    {
+        Time.timeScale = 0f;
+        PauseButton.SetActive(false);
+        audioManager.Play("Vitoria");
+        gameManager.Vitoria();
+    }
+
+    private void HandleItemPickup()
+    {
+        if (_hc.currentLifes <= 2)
         {
-            res = 1;
-            count_e = 5f;
-            Destroy(other.gameObject);
+            _hc.GainHealth(1);
         }
-        
-        if (other.CompareTag("Pontuador"))
+        else
         {
             Pontos.QtdPts += 2;
-            Destroy(other.gameObject);
         }
-        
-        if (other.CompareTag("Limite"))
-        {
-            Time.timeScale = 0f;
-            PauseButton.SetActive(false);
-            FindObjectOfType<AudioManager>().Play("Vitoria");
-            gameManager.Vitoria();
-        }
-        
-        if (other.CompareTag("Item"))
-        {
-            if (_hc.currentLifes <= 2)
-            {
-                _hc.GainHealth(1);
-            }
-            else
-            {
-                Pontos.QtdPts += 2;
-            }
-            FindObjectOfType<AudioManager>().Play("Item");
-            Destroy(other.gameObject);
-        }
-        
-        if (other.CompareTag("Obstaculo"))
-        {
-            if (res == 1)
-            {
-                res = 0;
-                Destroy(other.gameObject);
-            }
+        audioManager.Play("Item");
+    }
 
-            else if (res == 0)
-            {
-                _hc.TakeDamage(1);
-                df.tookDamage = true;
-                FindObjectOfType<AudioManager>().Play("PlayerHurt");
-                Destroy(other.gameObject);
-
-                getHit = 1;
-                count_D = 1f;
-            }
-        }
-        
-        if (other.CompareTag("InstaKill"))
+    private void HandleObstacleCollision()
+    {
+        if (hasShield)
         {
-            _hc.Instakill();
+            hasShield = false;
+            escudo.SetActive(false);
+        }
+        else
+        {
+            _hc.TakeDamage(1);
+            df.tookDamage = true;
+            audioManager.Play("PlayerHurt");
+
+            speedDown = true;
+            count_D = 1f;
         }
     }
 }
